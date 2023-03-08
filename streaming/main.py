b1d98bed0c0498016ab4584459b3c3f7bf7fd67c
suhaas-goog <126450878+suhaas-goog@users.noreply.github.com>
2023-03-08 15:40:07 +0530

    Update main.py

--- streaming/main.py
+++ streaming/main.py
@@ -48,11 +48,12 @@ def run():
   options = WritePubSubOptions(streaming=True)
 
   
-  #p = beam.Pipeline(options=options)
-  with beam.Pipeline(options=options) as p:
-    (p | "Read from PubSub" >> ReadFromPubSub(topic=input_topic)
-       | "EncodeString" >> ParDo(PubsubToPubsub())
-       | "Write to PubSub" >> WriteToPubSub(topic=options.topic))
+  p = beam.Pipeline(options=options)
+  #with beam.Pipeline(options=options) as p:
+  (p | "Read from PubSub" >> ReadFromPubSub(topic=input_topic)
+     | "EncodeString" >> ParDo(PubsubToPubsub())
+     | "Write to PubSub" >> WriteToPubSub(topic=options.topic))
+  p.run()  
 
 
 if __name__ == "__main__":
